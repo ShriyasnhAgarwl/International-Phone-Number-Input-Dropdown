72910dfda4efb1310b962a334eea118e8239d02d
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2024-01-19 11:23:19 +0530

    Update input.view.tsx
    aDDED SEARCH ITEM fILTER WITH THE SAME

--- input.view.tsx
+++ input.view.tsx
@@ -1,9 +1,9 @@
-<div
-                                        onClick={props.toggleDropdown}
-                                        className="cursor-pointer"
-                                    >
+<div>
                                         <div className="relative flex items-center bg-[#E7E7E7DB] text-sm font-normal border border-grey-500 rounded-lg">
-                                            <div className="flex items-center p-4 cursor-pointer w-[90px] hover:bg-[#489BF6] group hover:rounded-lg">
+                                            <div
+                                                onClick={props.toggleDropdown}
+                                                className="flex items-center p-4 cursor-pointer w-[90px] hover:bg-[#489BF6] group hover:rounded-lg"
+                                            >
                                                 <p className="font-[600] text-gray-700 group-hover:text-white">
                                                     {props.country}
                                                 </p>
@@ -14,65 +14,48 @@
                                                 <i className="fa fa-angle-down pl-1 group-hover:text-white"></i>
                                             </div>
                                             {props.isOpen && (
-                                                <div className=" absolute z-[9999]  mt-56  rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 h-[150px] overflow-y-auto">
-                                                    {/* <li
-                                                        className="block px-6 py-2 text-gray-700 text-center  hover:bg-[#489BF6] hover:text-white font-[600]  cursor-pointer rounded-md"
-                                                        value={"+91"}
-                                                        onClick={(e) =>
-                                                            props.togglePhone(
-                                                                "+" +
-                                                                    e.currentTarget.value.toString()
-                                                            )
+                                                <div className=" absolute z-[9999]  mt-56  rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 h-[150px]  w-[200px] overflow-y-auto">
+                                                    <input
+                                                        id="search-input"
+                                                        className="block w-full px-4 py-2 text-gray-800 border rounded-md  border-gray-300 focus:outline-none"
+                                                        type="text"
+                                                        placeholder="Search items"
+                                                        onChange={
+                                                            props.handleSearchInput
                                                         }
-                                                    >
-                                                        India (+91)
-                                                    </li> */}
-
-                                                    {data.map((item, i) => (
-                                                        <li
-                                                            className="block px-6 py-2 text-gray-700 text-center   hover:bg-[#489BF6] hover:text-white font-[600]  cursor-pointer rounded-md"
-                                                            value={
-                                                                item.dial_code
-                                                            }
-                                                            onClick={(e) => {
-                                                                props.togglePhone(
-                                                                    "+" +
-                                                                        e.currentTarget.value.toString()
-                                                                );
-                                                                props.toggleCountry(
-                                                                    item.code
-                                                                );
-                                                            }}
-                                                        >
-                                                            {item.name}(
-                                                            {item.dial_code})
-                                                        </li>
-                                                    ))}
-
-                                                    {/*<li
-                                                        className="block px-2 py-2 text-gray-700 text-center   hover:bg-[#489BF6] hover:text-white font-[600]  cursor-pointer rounded-md"
-                                                        value={"+1"}
-                                                        onClick={(e) =>
-                                                            props.togglePhone(
-                                                                "+" +
-                                                                    e.currentTarget.value.toString()
-                                                            )
-                                                        }
-                                                    >
-                                                        USA (+1)
-                                                    </li>
-                                                    <li
-                                                        className="block px-6 py-2 text-gray-700 text-center   hover:bg-[#489BF6] hover:text-white font-[600]  cursor-pointer rounded-md"
-                                                        value={"+971"}
-                                                        onClick={(e) =>
-                                                            props.togglePhone(
-                                                                "+" +
-                                                                    e.currentTarget.value.toString()
-                                                            )
-                                                        }
-                                                    >
-                                                        U.A.E (+971)
-                                                    </li> */}
+                                                    ></input>
+                                                    {data
+                                                        .filter((item) =>
+                                                            item.name
+                                                                .toLowerCase()
+                                                                .includes(
+                                                                    props.searchterm.toLowerCase()
+                                                                )
+                                                        )
+                                                        .map((item, i) => (
+                                                            <li
+                                                                key={i}
+                                                                className="block px-6 py-2 text-[14px] text-gray-700 text-center   hover:bg-[#489BF6] hover:text-white font-[600]  cursor-pointer rounded-md"
+                                                                value={
+                                                                    item.dial_code
+                                                                }
+                                                                onClick={(
+                                                                    e
+                                                                ) => {
+                                                                    props.togglePhone(
+                                                                        "+" +
+                                                                            e.currentTarget.value.toString()
+                                                                    );
+                                                                    props.toggleCountry(
+                                                                        item.code
+                                                                    );
+                                                                }}
+                                                            >
+                                                                {item.name}(
+                                                                {item.dial_code}
+                                                                )
+                                                            </li>
+                                                        ))}
                                                 </div>
                                             )}
                                             <input
